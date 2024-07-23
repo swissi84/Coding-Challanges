4cbdf9d3801ae67ae236c0c70929aaaf3f3db490
Andre Eggenschwiler <swissi1984@gmail.com>
2024-07-23 12:39:21 +0200

    true to Ja.  False to Nein

--- Woche 5 Tag 2/main.swift
+++ Woche 5 Tag 2/main.swift
@@ -19,23 +19,36 @@ class Tier {
         self.name = name
         self.alter = alter
     }
+    func beschreibung() {
+        print("Der Hund heisst \(name) und ist \(alter) alt.")
+        
+    }
 }
-
 
 class Hund:Tier {
     var lautBellen:Bool = true
     
     
-    func printInfo() {
-        print("Der Hund heisst \(name) und ist \(alter) alt. Bellt er laut: \(lautBellen)")
+    override func beschreibung() {
+        print("Der Hund heisst \(name) und ist \(alter) alt. Bellt er laut: \(lautBellenString())")
     }
+    
+    func lautBellenString() -> String {
+        
+        if lautBellen == true {
+            return "Ja"
+        } else {
+            return "Nein"
+        }
+    }
+    
+    
 }
 
 
 var hund1 = Hund(name: "Bello", alter: 11)
 
-
-hund1.printInfo()
+hund1.beschreibung()
 
 
 
@@ -43,36 +56,31 @@ print("\n---------------Coding Challange 5.2.2-----------------\n")
 
 
 
-class Tiere {
-    let name: String
-    var alter: Int
-    
-    init(name: String, alter: Int) {
-        self.name = name
-        self.alter = alter
-    }
-    
-    func beschreibung() {
-       print("Name: \(name), Alter: \(alter)")
-    }
-}
-
-
-class Katze:Tiere {
+class Katze:Tier {
     var schnurrt:Bool
     
-    init(name: String, alter: Int, schnurrt:Bool) {
+    
+    
+    init(name: String, alter: Int, schnurrt: Bool) {
         self.schnurrt = schnurrt
         super.init(name: name, alter: alter)
         
     }
-   
+    
     override func beschreibung() {
-        print("Name: \(name), Alter: \(alter) Schnurrt: \(schnurrt)")
+        print("Die Katze heisst \(name) und ist \(alter) alt. Schnurrt Sie: \(schnurrtString())")
+    }
+    
+    func schnurrtString() -> String {
+        
+        if schnurrt == true {
+            return "Ja"
+        } else {
+            return "Nein"
+        }
     }
 }
 
-
-var katze1 = Katze(name: "Luna", alter: 2, schnurrt: true)
+var katze1 = Katze(name: "Luna", alter: 2, schnurrt: false)
 
 katze1.beschreibung()
